cf58ec83203a21a172fabf9e22e09000a883a4b1
Anusree <sajeevananusree68@gmail.com>
2025-07-10 21:31:44 +0530

    second change

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -6,29 +6,8 @@ import Navbarcomp from './components/header/Navbar'
 
 
 const productcontext = createContext()
-// const Routing = () => {
-//   const [product, setproduct] = useState([])
-//   const [selectedproduct, setselectedproduct] = useState({})
-//   const [filterdata, setfilterdata] = useState(null)
-//      const [navbar, setnavbar] = useState(true)
-//   const [allProducts, setallProducts] = useState([])
-//   return (
-//     <div>
-//       <productcontext.Provider value={{product, setproduct,selectedproduct, setselectedproduct, filterdata, setfilterdata, allProducts, setallProducts}}>
-//       <BrowserRouter>
-//        {navbar ? <Navbarcomp /> : null}
-//         <Routes>
-//             <Route path="/detail" element={<Detail />} />
-//           <Route path="/" element={<Product />} />
-//         </Routes>
-//       </BrowserRouter>
-//       </productcontext.Provider>
-//     </div>
-//   )
-// }
 
-// export default Routing
-// export { productcontext } // Export the context for use in other components
+
 const Routing = () => {
   const [product, setproduct] = useState([]);
   const [selectedproduct, setselectedproduct] = useState({});
